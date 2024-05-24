fd75fa6a959b40b83e7eac3dd420d80c85434e1f
AriGasper <arishagura@gmail.com>
2024-05-24 22:24:26 +0300

    Создание интерфейса команды

--- 3_TP/src/core/commands/IChartCommand.cs
+++ 3_TP/src/core/commands/IChartCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _3_TP.src.core.presenter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace _3_TP.src.core.commands
 {
-    internal interface IChartCommand
+    public interface IChartCommand
     {
+        /// <summary>
+        /// Выполнить команду.
+        /// </summary>
+        /// <param name="presenter">Презентер, отвечающий за отображение на экране.</param>
+        void Run(IChartPresenter presenter);
     }
 }
